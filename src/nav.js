718d9b34a340ebc0d7410a394234663fe4a837b9
Jorge <hernandezj0174@gmail.com>
2023-08-27 15:15:06 -0500

    Major changes
    Added mobile navigation menu, made styling changing, reorganized project structure and added more content to webpage.

---
+++ src/nav.js
@@ -0,0 +1,26 @@
+const primaryNav = document.querySelector('.primary-navigation');
+const navToggle = document.querySelector('.mobile-nav-toggle');
+
+navToggle.addEventListener('click', () => {
+    const visibility = primaryNav.getAttribute('data-visible');
+
+    if (visibility === "false") {
+        primaryNav.setAttribute('data-visible', true);
+        navToggle.setAttribute('aria-expanded', true);
+    } else {
+        primaryNav.setAttribute('data-visible', false);
+        navToggle.setAttribute('aria-expanded', false);
+    }
+});
+
+const primaryHeader = document.querySelector('.primary-header');
+const scrollWatcher = document.createElement('div');
+
+scrollWatcher.setAttribute('data-scroll-watcher', '');
+primaryHeader.before(scrollWatcher);
+
+const navObserver = new IntersectionObserver((entries) => {
+    primaryHeader.classList.toggle('sticking', !entries[0].isIntersecting)
+}, {rootMargin: "60px 0px 0px 0px"});
+
+navObserver.observe(scrollWatcher)
